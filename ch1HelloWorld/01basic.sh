50380772a493fcf260487fcfa15ee28fc29a1549
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-05-04 12:05:10 +0530

    echo & comments

---
+++ ch1HelloWorld/01basic.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo "Hello Buddy!"
+
+# cat /etc/shells
+# List of acceptable shells for chpass(1).
+# Ftpd will not allow users to connect who are not using
+# one of these shells.
+
+# /bin/bash
+# /bin/csh
+# /bin/dash
+# /bin/ksh
+# /bin/sh
+# /bin/tcsh
+# /bin/zsh
+
+# ls -ltr
+# total 24
+# -rwxr-xr-x  1 rajan  staff  32 May  3 07:21 sih.sh*
+# -rwxr-xr-x@ 1 rajan  staff  91 May  3 07:26 start*
+# -rw-r--r--@ 1 rajan  staff  31 May  4 11:21 01basic.sh
+
+# chmod +x 01basic.sh 
---
+++ ch1HelloWorld/02basics.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Cecking Comments"
+
+# This is a single line comment
+
+<<comment
+This
+is
+Multi
+Line Comments
+comment
